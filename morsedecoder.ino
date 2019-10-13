0de3dd6d7a550ec8ea6072716a87d48a4c754747
Per Juchtmans <per.juchtmans@digipolis.gent>
2019-10-13 21:20:42 +0200

    Letters and digits in Morse code.

---
+++ morsedecoder.ino
@@ -0,0 +1,61 @@
+/*
+International Morse Code
+https://en.wikipedia.org/wiki/Morse_code
+
+    1. The length of a dot is 1 unit
+    2. A dash is 3 units
+    3. The space between parts of the same letter is 1 unit
+    4. The space between letters is 3 units
+    5. The space between words is 7 units
+*/
+
+
+String letters[] = {
+    ".-",
+    "-...",
+    "-.-.",
+    "-..",
+    ".",
+    "..-.",
+    "--.",
+    "....",
+    "..",
+    ".---",
+    "-.-",
+    ".-..",
+    "--",
+    "-.",
+    "---",
+    ".--.",
+    "--.-",
+    ".-.",
+    "...",
+    "-",
+    "..-",
+    "...-",
+    ".--",
+    "-..-",
+    "-.--",
+    "--.."
+};
+String digits = {
+    "-----",
+    ".----",
+    "..---",
+    "...--",
+    "....-",
+    ".....",
+    "-....",
+    "--...",
+    "---..",
+    "----.",
+};
+
+void setup()
+{
+}
+
+
+void loop()
+{
+}
